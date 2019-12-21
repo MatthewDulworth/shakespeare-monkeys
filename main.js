453ed8846788597ce7eb54d4257e21455cff0678
Matthew Dulworth <mhdulworth@gmail.com>
2019-12-21 17:35:15 -0600

    Fixed bugs in genome generation

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
+// ----------- Constants ----------- //
 const shakespeare = "this is a test";
 
 // ----------- Genome ----------- //
 // holds the genetic information of each monkey
 class Genome {
-
    constructor(length) {
       this.length = length;
-      this.string = generateRandomString(length);
+      this.string = this.generateRandomGenome(length);
    }
 
-   // generates a random string of charecters up to length
-   generateRandomString(length) {
+   // generates a random string of nucleotides up to length
+   generateRandomGenome(length) {
       let str = "";
       for (let i = 0; i < length; i++) {
-         str += this.generateRandomChar();
+         str += this.getRandomNucleotide();
       }
       return str;
    }
 
-   // generates a random charecter from between ascii codes 32 & 126, inclusive
-   generateRandomChar() {
-      let num = Math.floor(Math.random() * (32 - 126 + 1)) + 32;
-      return String.fromCharCode(num);
+   // get a random charecter from the nucleotide string
+   getRandomNucleotide()
+   {
+      let index = Math.floor(Math.random() * Genome.nucleotides().length);
+      return Genome.nucleotides().charAt(index);
+   }
+
+   // the possible nucleotides that make up the genome 
+   static nucleotides()
+   {
+      return "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789{}[]|~`!@#$%^&*()-_=+<>, .?/:;\'\"\\";
    }
 }
 
@@ -35,8 +42,7 @@ class Monkey {
    }
 
    // each correct letter is +1 fitness, each correct word is +2 fitness
-   calculateFitness()
-   {
+   calculateFitness() {
       let fitness = 0;
       return fitness;
    }
@@ -45,15 +51,25 @@ class Monkey {
 // ----------- Population ----------- //
 // the set of monkeys and the methods to evolve them
 class Population {
-   constructor() {
-      this.monkeys = initialPopulation();
+   constructor(size, genomeLength) {
+      this.size = size;
+      this.monkeys = this.initialPopulation(size, genomeLength);
    }
 
-   initialPopulation() {
-      
+   initialPopulation(size, genomeLength) {
+      let monkeys = [];
+      for (let i = 0; i < size; i++) {
+         monkeys.push(new Monkey(genomeLength));
+      }
+      return monkeys;
    }
 }
 
 
 // ----------- Main ----------- //
 // execution starts here
+let population = new Population(5, 100);
+
+population.monkeys.forEach(monkey => {
+   console.log(monkey.genome.string);
+});
