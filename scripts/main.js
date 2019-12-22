6c696c4bfbe91b1bd88bdcb0059cf3c6d467da37
Matthew Dulworth <mhdulworth@gmail.com>
2019-12-22 12:56:09 -0600

    restructured files

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
 // ----------- Imports ----------- //
-import * as StringTools from "./tools.js";
+import * as StringTools from "./string-tools.js";
 import {Population, Genome} from "./genetics.js";
 
 // ----------- Constants ----------- //
-const goal = "Hello There! general Kenobi!";
-
+const goal = "Hello There! General Kenobi!";
 
 // ----------- Main ----------- //
 // execution starts here
 let goalData = StringTools.analyzeString(goal, Genome.nucleotides());
-let population = new Population(1, goal);
+let population = new Population(5, goal);
+
+population.display();
