63df62ed328209d778de966fc04e32fe5046f8d2
Matthew Dulworth <mhdulworth@gmail.com>
2019-12-24 14:05:55 -0600

    Added mating methods n stuff, untested

--- scripts/monkey.js
+++ scripts/monkey.js
@@ -2,11 +2,54 @@
 // each monkey has a genome and a fitness score
 export default class Monkey {
 
-   constructor(target) {
-      this.genome = Monkey.generateRandomGenome(target.length);
-      this.fitness = Monkey.calculateFitness(target, this.genome);
+   constructor(genome) {
+      this.genome = genome;
    }
 
+   // randomizes the genome of the monkey
+   randomizeGenome(length) {
+      this.genome = "";
+      for (let i = 0; i < length; i++) {
+         this.genome += Monkey.getRandomNucleotide();
+      }
+   }
+
+   // calculates the fitness of the monkey
+   calculateFitness(target) {
+      let i = 0, l = this.genome.length;
+      this.fitness = 0;
+
+      for (i; i < l; i++) {
+         if (this.genome.charAt(i) != target.charAt(i)) {
+            this.fitness++;
+         }
+      }
+      return this.fitness;
+   }
+
+   // returns the offspring of the two monkeys
+   static mate(monkey1, monkey2, genome_length) {
+      let offspring = new Monkey(target);
+
+      for (let i = 0; i < genome_length; i++) {
+         let parent1 = Math.random < 0.5;
+
+         if (Math.random() < this.mutation_chance) {
+            offspring.genome += Monkey.getRandomNucleotide();
+         }
+         else if (parent1) {
+            offspring.genome += monkey1.genome.charAt(i);
+         }
+         else if (!parent1) {
+            offspring.genome += monkey2.genome.charAt(i);
+         }
+      }
+
+      offspring.calculateFitness();
+      return offspring;
+   }
+
+   // compare two monkeys based on fitness
    static compare(a, b) {
       if (a.fitness < b.fitness) {
          return 1;
@@ -19,28 +62,10 @@ export default class Monkey {
       }
    }
 
-   static generateRandomGenome(length) {
-      let str = "";
-      for (let i = 0; i < length; i++) {
-         str += Monkey.getRandomNucleotide();
-      }
-      return str;
-   }
-
+   // returns a random nucleotide
    static getRandomNucleotide() {
       let nucleotides = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789{}[]|~`!@#$%^&*()-_=+<>, .?/:;\'\"\\";
       let index = Math.floor(Math.random() * nucleotides.length);
       return nucleotides.charAt(index);
-   }
-
-   static calculateFitness(target, genome) {
-      let fitness = 0, l = genome.length;
-
-      for (let i = 0; i < l; i++) {
-         if (genome.charAt(i) != target.charAt(i)) {
-            fitness++;
-         }
-      }
-      return fitness;
    }
 }
--- scripts/population.js
+++ scripts/population.js
@@ -15,9 +15,33 @@ export default class Population {
       this.mating_population = this.calculateMatingPopulation(size, mating_percentage);
    }
 
+   // generates a random initial population
+   initialPopulation(size, target) {
+      let monkeys = [];
+      for (let i = 0; i < size; i++) {
+         let monkey = new Monkey(target);
+         monkey.randomizeGenome(target.length);
+         monkey.calculateFitness(target);
+         monkeys.push(monkey);
+      }
+      return this.sortMonkeys(monkeys);
+   }
+
    // creates a new generation of monkeys from the last one
    newGeneration() {
-      let new_generation = this.mateMonkeys(this.selectMatingMonkeys());
+      let new_generation = [];
+      let selected_monkeys = this.selectMatingMonkeys();
+
+      while (selected_monkeys.length > 0) {
+         let monkey1 = this.getRandomMonkey(selected_monkeys);
+         this.removeMonkey(monkey1, selected_monkeys);
+
+         let monkey2 = this.getRandomMonkey(selected_monkeys);
+         this.removeMonkey(monkey2, selected_monkeys);
+
+         new_generation.push(Monkey.mateMonkeys(monkey1, monkey2, this.target.length));
+      }
+
       return this.sortMonkeys(new_generation);
    }
 
@@ -25,15 +49,37 @@ export default class Population {
    selectMatingMonkeys() {
       let selected_monkeys = [];
 
-      for (let i = 0; i < this.mating_population; i++) {
-
+      for (let i = 0; i < this.size; i++) {
+         if (Math.random() < this.mating_chance) {
+            selected_monkeys.push(this.monkeys[i]);
+         }
+         if (selected_monkeys.length == this.mating_population) {
+            return selected_monkeys;
+         }
       }
-      return selected_monkeys;
+
+      // if not enough moneys are selected, reselect
+      if (selected_monkeys.length != this.mating_population) {
+         return this.selectMatingMonkeys();
+      }
    }
 
-   // mates the selected monkeys
-   mateMonkeys() {
+   // removes a given monkey from a given array, returns true if successful
+   removeMonkey(monkey, monkeys) {
+      let i = 0, l = monkeys.length;
+      for (i; i < l; i++) {
+         if (monkey == monkeys[i]) {
+            monkeys.splice(i, 1);
+            return true;
+         }
+      }
+      return false;
+   }
 
+   // gets a random monkey from a given array
+   getRandomMonkey(monkeys) {
+      let index = Math.floor(Math.random() * monkeys.length);
+      return monkeys[index];
    }
 
    // sorts the population by fitness
@@ -42,24 +88,6 @@ export default class Population {
       sorted.sort(Monkey.compare);
       sorted.reverse();
       return sorted;
-   }
-
-   // generates a random initial population
-   initialPopulation(size, target) {
-      let monkeys = [];
-      for (let i = 0; i < size; i++) {
-         monkeys.push(new Monkey(target));
-      }
-      return this.sortMonkeys(monkeys);
-   }
-
-   // finds monkeys with the best fitness scores
-   getFittest(num) {
-      let fittest = [];
-      for (let i = 0; i < num; i++) {
-         fittest.push(this.monkeys[i])
-      }
-      return fittest;
    }
 
    // calculates the number of monkeys in a generation that will mate
